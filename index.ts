653be76cdeab1bf45f1f89a733641f7bca7c795e
Alejandra Colcha <da.colcha@hotmail.com>
2023-10-30 22:31:16 -0500

    Mejora del prompt y de sus parametros

--- index.ts
+++ index.ts
@@ -1,34 +1,23 @@
-import examples from './examples';
-
 const cohere = require('cohere-ai');
-cohere.init('tPgdL0VP7EbnQiEkymbVuZto6DQlGHDNzq3HnYTh')
+cohere.init('tPgdL0VP7EbnQiEkymbVuZto6DQlGHDNzq3HnYTh'); 
 
-const inputs = ['Quiero cambiar mi contraseña'];
+const inputs = 'cómo debo separar los residuos en mi casa';
+//qué colores debo usar para separar los residuos en mi casa 
+//beneficios de reciclar
+//como debo separar los residuos en mi casa
+//consejos para empezar a reciclar
+//beneficios de andar en bicicleta
 
 (async () => {
-  const response = await cohere.classify({
-    inputs: inputs,
-    examples: examples,
+  const response = await cohere.generate({
+    model: 'command',
+    prompt: `You are a "recycling" and "environment" expert willing to answer questions
+    about best practices in recycling, waste management, biodiversity conservation or
+    any other topic related to the environment. Answer the following question: ${inputs}.
+    Provide a short, precise and useful answer in "spanish".`,
+    temperature:0.3, 
+    k :75, 
+    p:0.9
   });
-  console.log(response);
+  console.log(response.body.generations[0].text);
 })();
-
-// const cohere = require('cohere-ai');
-// cohere.init('tPgdL0VP7EbnQiEkymbVuZto6DQlGHDNzq3HnYTh'); 
-
-// const inputs = '¿Cuáles son los beneficios del reciclaje?';
-// //¿Cómo puedo educar a mis hijos sobre la importancia del reciclaje?
-// //¿Cuáles son los beneficios del reciclaje?
-// //¿Cómo puedo empezar a reciclar en mi área?
-// //¿Cómo puedo reducir mi huella de carbono personal?
-
-// (async () => {
-//   const response = await cohere.generate({
-//     model: 'command',
-//     prompt: `Eres un experto en reciclaje y cuidado del medioambiente. ${inputs}. Usa una respuesta corta y en español`,
-//     max_tokens: 300,
-//     temperature: 0.3, 
-//     k :50, 
-//   });
-//   console.log(response.body.generations[0].text);
-// })();
