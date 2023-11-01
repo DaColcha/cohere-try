db4d5f04746bb4d409bea4c62cbbef60c6fee102
Alejandra Colcha <da.colcha@hotmail.com>
2023-11-01 10:36:36 -0500

    Simplificación

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import fitParams  from './params';
-
 const cohere = require('cohere-ai');
 cohere.init('tPgdL0VP7EbnQiEkymbVuZto6DQlGHDNzq3HnYTh'); 
 
@@ -12,7 +10,17 @@ const inputs = 'recomendaciones para empezar a andar en bicicleta';
 
 (async () => {
   //genera la respuesta a la solicitud
-  const response = await cohere.generate(fitParams(inputs));
+  const response = await cohere.generate({
+    model: 'command',
+    prompt: `You are a "recycling" and "cycling" expert willing to answer questions
+    about best practices in recycling, waste management, biodiversity conservation or advices 
+    to start cycling and any other topic related to the environment and cycling. Answer the following question: ${inputs}.
+    Provide a short, precise and useful answer in "spanish".`,
+    temperature:0.3, 
+    k :72, 
+    p:0.9
+    }
+  );
 
   //imprime la respuesta en consola 
   console.log(response.body.generations[0].text);
--- src/types/params.ts
+++
@@ -1,7 +0,0 @@
-export interface CohereParams {
-    model: string,
-    prompt: string,
-    temperature: number,
-    k: number,
-    p: number
-}
